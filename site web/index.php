88c02db4a7f7d3596f7d43567e4ba2eaad6fd354
Tiffany <tiffany133098@gmail.com>
2019-09-05 11:41:51 +0200

    communication avec bdd

--- site web/index.php
+++ site web/index.php
@@ -8,4 +8,20 @@ catch (Exception $e)
         die('Erreur : ' . $e->getMessage());
 }
 
+$reponse = $bdd->query('select code from meridien');
+while ($donnees = $reponse->fetch())
+{
+?>
+    <p>
+        <?php echo $donnees['code']; ?><br />
+    </p>
+
+<?php
+
+}
+
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+
 ?>
