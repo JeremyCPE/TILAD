19ea1ad402b44d0f1c02366feed3f6338e595a37
Jeremy Mohariry <jeremy.mohariry@vinci-energies.net>
2019-10-03 14:54:06 +0200

    Suppression Dossier Site Web
    Suppression dossier site web

--- appli/site web/utilisateur.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `idS` int(11) NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL ,
-  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `adresse` varchar(100) COLLATE utf8mb4_unicode_ci ,
-  `nom` varchar(30) COLLATE utf8mb4_unicode_ci ,
-  `prenom` varchar(30) COLLATE utf8mb4_unicode_ci ,
-  PRIMARY KEY (`idS`)
-);
